91572a3f92db671dedb44a39470e02d91985d701
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-16 21:17:58 -0300

    Capítulo 1 - concluído

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
+
+function FormularioCadastro({aoEnviar, validarCPF}) {
+
+  return (
+    <>
+    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
+    <DadosUsuario />
+    <DadosEntrega />
+    </>
+  );
+}
+
+export default FormularioCadastro;
