7786999827647b02b7f7c55b0b1419004718ae9b
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-18 01:49:14 -0300

    Projeto Concluído

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,29 +1,14 @@
 import React, { useState, useContext } from "react";
 import { Button, TextField } from "@material-ui/core";
 import ValidacoesCadastro from "../../contexts/ValidacoesCadastro";
+import useErros from "../../hooks/useErros";
 
 function DadosUsuario({ aoEnviar }) {
     const [email, setEmail] = useState("");
     const [senha, setsenha] = useState("");
-    const [erros, setErros] = useState({ senha: { valido: true, texto: "" }});
-
     const validacoes = useContext(ValidacoesCadastro)
+    const [erros, validarCampos, possoEnviar] = useErros(validacoes);
 
-    function validarCampos(event) {
-        const { name, value } = event.target;
-        const novoEstado = { ...erros }
-        novoEstado[name] = validacoes[name](value);
-        setErros(novoEstado);
-    }
-
-    function possoEnviar() {
-        for (let campo in erros) {
-            if (!erros[campo].valido) {
-                return false;
-            }
-        }
-        return true;
-    }
     return (
         <form onSubmit={(event) => {
             event.preventDefault();
